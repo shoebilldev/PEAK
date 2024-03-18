4593546a441d2da23fed8a3500f5c14ccd8ca43c
shoebill <cmcjnz@gmail.com>
2024-03-18 01:40:38 +0000

    remove old unused code

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,5 @@
 use hframe::Aware;
 
-const IFRAME: &str = r#"
-<iframe src="https://www.example.com/"></iframe>
-"#;
-
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
